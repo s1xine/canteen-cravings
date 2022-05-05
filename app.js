ba1464393eb734d64f8bd95a6bbaa6e602240cdb
Prajot Baloliya <prajotbaloliya@gmail.com>
2022-05-06 03:03:04 +0530

    changed port

--- app.js
+++ app.js
@@ -101,6 +101,10 @@ app.get("/logindex", function (req, res) {
   res.render("logindex");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function () {
+  console.log("Server has started successfully");
 });
